9c5717a7bcb0a84f369909764ee21fc52872f42b
= <=>
2024-11-12 14:27:22 +0700

    JS 10 PBO - Prak 1

--- Pegawai.java
+++ Pegawai.java
@@ -1,17 +1,20 @@
 public class Pegawai {
+
     public String nip;
     public String nama;
 
-    public Pegawai() {
+    public Pegawai(){
+
     }
 
-    public Pegawai(String nip, String nama) {
+    public Pegawai (String nip, String nama){
         this.nip = nip;
         this.nama = nama;
+
     }
 
-    public void displayInfo() {
-        System.out.println("NIP: " + nip);
-        System.out.println("Nama: " + nama);
+    public void displayInfo(){
+        System.out.println("NIP         : " + nip);
+        System.out.println("Nama        : " + nama);
     }
 }
